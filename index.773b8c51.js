72d6f463ab5874f167af1cc37b6475c648597474
kammil1 <139874069+kammil1@users.noreply.github.com>
2023-11-22 21:40:55 +0000

    Deploying to gh-pages from @ kammil1/simply-chocolate@1161962e511761152c09e655916792dbac5cbd8b 🚀

---
+++ index.773b8c51.js
@@ -0,0 +1,3 @@
+(()=>{let e={openModalBtn:document.querySelector("[subscribe-data-modal-open]"),closeModalBtn:document.querySelector("[subscribe-data-modal-close]"),modal:document.querySelector("[subscribe-data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();//# sourceMappingURL=index.773b8c51.js.map
+
+//# sourceMappingURL=index.773b8c51.js.map
