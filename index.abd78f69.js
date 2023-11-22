a1ddd380099826daa37154018f868f2867221425
kammil1 <139874069+kammil1@users.noreply.github.com>
2023-11-22 21:34:40 +0000

    Deploying to gh-pages from @ kammil1/simply-chocolate@6455465be2d96a9f18002378d0557720afef0e67 🚀

---
+++ index.abd78f69.js
@@ -0,0 +1,3 @@
+(()=>{let e={openModalBtn:document.querySelector("[reviews-data-modal-open]"),closeModalBtn:document.querySelector("[reviews-data-modal-close]"),modal:document.querySelector("[reviews-data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();//# sourceMappingURL=index.abd78f69.js.map
+
+//# sourceMappingURL=index.abd78f69.js.map
